161ea7233a8030b5d6a5a2e630c6d8f857e845fa
MonaSweataSK <monasweatask@gmail.com>
2026-02-01 18:22:30 +0530

    sort function

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [ quotesData, setQuotesData ] = useState([]);
   const [ search, setSearch] = useState('');
   const [ debouncedSearch, setDebouncedSearch ] = useState('');
+  const [ isSort, setIsSort ] = useState(false);
 
   useEffect(() => {
     fetch('https://dummyjson.com/quotes')
@@ -21,14 +22,21 @@ function App() {
   }, [search]);
 
   const filteredQuotes = useMemo(() => {
-    return quotesData.filter((quote) => {
+    const temp = [...quotesData];
+    if(isSort) {
+      temp.sort((a,b) => {
+        return a.quote.localeCompare(b.quote);
+      });
+    } 
+    return ( isSort ? temp : quotesData).filter((quote) => {
       return quote.quote.toLowerCase().includes(debouncedSearch.toLowerCase()) || quote.author.toLowerCase().includes(debouncedSearch.toLowerCase())
     });
-  }, [quotesData, debouncedSearch]);
+  }, [quotesData, debouncedSearch, isSort]);
 
   return (
     <div className={`App-${Date.now()}`}>
       <input placeholder='Search....' value={search} onChange={(e) => setSearch(e.target.value)}></input>
+      <button onClick={()=>setIsSort(!isSort)}>Sort</button>
       {filteredQuotes.map((quote) => (
           <div key={quote.id}>
              <p>{quote.quote}</p>
